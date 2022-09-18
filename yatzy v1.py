e9e6b444b9e33df58af3cf07a3f27470cceb263e
erigra <erikgrande71@gmail.com>
2022-09-18 13:02:38 +0200

    poeng

--- yatzy v1.py
+++ yatzy v1.py
@@ -13,28 +13,28 @@ class Poengfelt():
         self.brukt = brukt
 
 
-enere = Poengfelt("Enere:    ", 0 , False )
-toere = Poengfelt("Toere:    ", 0 , False )
-treere = Poengfelt("Treere:   ", 0 , False )
-firere = Poengfelt("Firere:   ", 0 , False )
-femere = Poengfelt("Femere:   ", 0 , False )
-seksere = Poengfelt("Seksere:  ", 0 , False )
-delestrek = Poengfelt ("_______________________________", 0, True)
-delsum = Poengfelt("Delsum    ", 0 , False )
-bonus = Poengfelt("Bonus:    ", 0 , False )
-ett_par = Poengfelt("Ett par:  ", 0 , False )
-to_par = Poengfelt("To par:   ", 0 , False )
-tre_like= Poengfelt("Tre like: ", 0 , False )
-fire_like = Poengfelt("Fire like:", 0 , False )
-hus = Poengfelt("Hus:      ", 0 , False )
-straight = Poengfelt("Straight: ", 0 , False )
-yatzy = Poengfelt("Yatzy!:   ", 0 , False )
-sjanse = Poengfelt("Sjanse    ", 0 , False )
-sluttstrek = Poengfelt ("_______________________________", 0, True)
-sum = Poengfelt("Sum:       ", 0 , False )
+enere0 = Poengfelt("Enere:     ", 0 , False )
+toere1 = Poengfelt("Toere:     ", 0 , False )
+treere2 = Poengfelt("Treere:   ", 0 , False )
+firere3 = Poengfelt("Firere:    ", 16 , True )
+femere4 = Poengfelt("Femere:    ", 0 , False )
+seksere5 = Poengfelt("Seksere:   ", 0 , False )
+delestrek6 = Poengfelt ("_______________________________", 0, False)
+delsum7 = Poengfelt("Delsum     ", 0 , False )
+bonus8 = Poengfelt("Bonus:     ", 0 , False )
+ett_par9 = Poengfelt("Ett par:   ", 0 , False )
+to_par10 = Poengfelt("To par:    ", 0 , False )
+tre_like11= Poengfelt("Tre like:  ", 0 , False )
+fire_like12 = Poengfelt("Fire like: ", 0 , False )
+hus13 = Poengfelt("Hus:       ", 0 , False )
+straight14 = Poengfelt("Straight:  ", 0 , False )
+yatzy15 = Poengfelt("Yatzy!:    ", 0 , False )
+sjanse16 = Poengfelt("Sjanse     ", 0 , False )
+sluttstrek17 = Poengfelt ("_______________________________", 0, False)
+sum18 = Poengfelt("Sum:        ", 0 , False )
 
 # En Tuple som har 19 elementer (0-18)
-poeng_skjema=(enere, toere, treere, firere, femere, seksere, delestrek, delsum, bonus, ett_par, to_par, tre_like, fire_like, hus, straight, yatzy, sjanse, sluttstrek, sum)
+poeng_skjema=(enere0, toere1, treere2, firere3, femere4, seksere5, delestrek6, delsum7, bonus8, ett_par9, to_par10, tre_like11, fire_like12, hus13, straight14, yatzy15, sjanse16, sluttstrek17, sum18)
 
 
 # Funksjoner:
@@ -43,7 +43,10 @@ poeng_skjema=(enere, toere, treere, firere, femere, seksere, delestrek, delsum, 
 def vis_poengskjema(poengskjema):
     print("POENGSKJEMA: ")
     for n in range (19):
-        print(poengskjema[n].navn,poengskjema[n].verdi)
+        if poengskjema[n].brukt==False:
+            print(poengskjema[n].navn)
+        else:    
+            print(poengskjema[n].navn,poengskjema[n].verdi)
 
 
 # Skriver ut en liste med terningverdiene og om terningen er valgt å beholde
@@ -94,53 +97,53 @@ vis_poengskjema(poeng_skjema)
 
 print("Yatzy spill!")
 
-# 1 kast ----------------------------------------
-input("Trykk RETURN for å kaste ditt første kast!")
-antall_kast = 1
+while True:
 
-t0 = Terning(random.randint(1,6), False)
-t1 = Terning(random.randint(1,6), False)
-t2 = Terning(random.randint(1,6), False)
-t3 = Terning(random.randint(1,6), False)
-t4 = Terning(random.randint(1,6), False)
+    # En kasterunde starter her
+    # 1 kast 
+    input("Trykk RETURN for å kaste ditt første kast!")
+    antall_kast = 1
 
-terningsett = [t0,t1,t2,t3,t4]
+    # Setter opp/kaster nye terninger for denne runden som første kast
+    t0 = Terning(random.randint(1,6), False)
+    t1 = Terning(random.randint(1,6), False)
+    t2 = Terning(random.randint(1,6), False)
+    t3 = Terning(random.randint(1,6), False)
+    t4 = Terning(random.randint(1,6), False)
+    terningsett = [t0,t1,t2,t3,t4]
 
-print()
-print("------------------------- Resultat 1. kast: -----------------------------------")
-velge_terninger()
-
-
-# 2 kast ---------------------------------------
-antall_kast=2
-kast_terninger_igjen(terningsett)
-
-print()
-print("--------------------  Resultat 2. kast: -------------------------")
-
-velge_terninger()
+    print()
+    print("------------------------- Resultat 1. kast: -----------------------------------")
+    velge_terninger()
 
 
-# 3 kast ---------------------------------------
-antall_kast=3
-kast_terninger_igjen(terningsett)
+    # 2 kast
+    antall_kast=2
+    kast_terninger_igjen(terningsett)
+    print()
+    print("--------------------  Resultat 2. kast: -------------------------")
+    velge_terninger()
 
-print()
-print("--------------------  Resultat 3. kast: -------------------------")
-vis_terningoversikt(terningsett)
+
+    # 3 kast 
+    antall_kast=3
+    kast_terninger_igjen(terningsett)
+    print()
+    print("--------------------  Resultat 3. kast: -------------------------")
+    vis_terningoversikt(terningsett)
 
 
-# Ferdig med å kaste terninger -------------------------------------------------------
+    # Ferdig med å kaste terninger denne runden ::::::::::::::::::::
 
-# Lager en ny liste med bare terningverdier
-endelige_verdier = []
-for n in range(5):
-    endelige_verdier.append(terningsett[n].verdi)
+    # Lager en ny liste med bare terningverdier for denne runden
+    endelige_verdier = []
+    for n in range(5):
+        endelige_verdier.append(terningsett[n].verdi)
 
-# Lager en liste med antallet av hver verdi  
-antall_oversikt = []
-for n in range(1, 7):
-    antall_oversikt.append(endelige_verdier.count(n))
+    # Lager en liste med antallet av hver verdi for denne runden  
+    antall_oversikt = []
+    for n in range(1, 7):
+        antall_oversikt.append(endelige_verdier.count(n))
 
 
 
